4db2c06919fc4bd265af9418032b5a0fab68aa1e
Cullen Jett <cullenjett@gmail.com>
2014-04-30 12:17:37 -0600

    Finished blackjack.rb

--- course1/blackjack.rb
+++ course1/blackjack.rb
@@ -2,27 +2,32 @@
 
 require 'pry'
 
-def calculate_total(hand) # [['5', 'Hearts'], ['2', 'Spades']...]
+def calculate_total(hand)
+  arr = hand.map { |e| e[0] }
+
   total = 0
-  
-  hand.each do |card|
-    case card.first
-    when 'J'
+  arr.each do |value|
+    if value == 'A'
+      total += 11
+    elsif value.to_i == 0 # J, Q, K
       total += 10
-    when 'Q'
-      total += 10
-    when 'K'
-      total += 10
-    when 'A'
-      total += 11 # FIX THIS: 'A' is worth 11 unless your total goes over 21, then it's worth 1
+    else
+      total += value.to_i
     end
-    total += card.first.to_i
   end
+
+  #correct for aces
+  arr.select{ |e| e == 'A' }.count.times do
+    total -= 10 if total > 21
+  end
+
   total
 end
 
 
-#----- create a deck -----
+#----- Start game, create deck -----
+
+puts "---> Welcome to the blackjack table <---"
 
 num_array = [ '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ]
 suit_array = [ 'Hearts', 'Diamonds', 'Spades', 'Clubs' ]
@@ -31,7 +36,7 @@ deck = num_array.product(suit_array)
 deck.shuffle!
 
 
-#----- deal 2 cards to player and dealer -----
+#----- Deal 2 cards to player and dealer -----
 
 player_hand = []
 dealer_hand = []
@@ -41,40 +46,97 @@ dealer_hand = []
   dealer_hand << deck.pop
 end
 
+player_total = calculate_total(player_hand)
+dealer_total = calculate_total(dealer_hand)
 
-#----- show cards, get input from player -----
 
-puts "---> Welcome to the table. #{deck.count} cards are shuffled and ready to deal! <---"
+#----- Show cards, get input from player -----
 
 puts "Your hand: #{player_hand[0]} and #{player_hand[1]}."
-puts "YOUR TOTAL: " + calculate_total(player_hand).to_s
-
+puts "YOUR TOTAL: #{player_total}"
 puts ''
-
 puts "The dealer has: #{dealer_hand[0]} and #{dealer_hand[1]}."
-puts "DEALER'S TOTAL: " + calculate_total(dealer_hand).to_s
-
+puts "DEALER'S TOTAL: #{dealer_total}"
 puts ''
 
-puts "What would you like to do? 1.Hit 2.Stay"
-choice = gets.chomp
-choice.downcase!
 
-#----- actions for when player hits or stays -----
+#----- Player's turn -----
 
-while choice == '1'
-  player_hand << deck.pop
-  puts "You hand: #{player_hand}"
-  puts "Your TOTAL: " + calculate_total(player_hand).to_s
+if player_total == 21
+  puts "BLACKJACK! YOU WIN!"
+  exit
+end
+
+while player_total < 21
   puts "What would you like to do? 1.Hit 2.Stay"
   choice = gets.chomp
-  choice.downcase!
+
+  if !['1', '2'].include?(choice)
+    puts "Error: you must enter 1 or 2"
+    next
+  end
+
+  if choice == '2'
+    puts "You chose to stay"
+    break
+  end
+
+  #hit
+  new_card = deck.pop
+  puts "You drew #{new_card}"
+  player_hand << new_card
+  player_total = calculate_total(player_hand)
+  puts "Your hand is now #{player_hand}"
+  puts "YOUR TOTAL: #{player_total}"
+
+  if player_total == 21
+    puts "BLACKJACK! YOU WIN!"
+    exit
+  elsif player_total > 21
+    puts "BUST! YOU LOSE!"
+    exit
+  end
 end
 
-while choice == '2'
-  #END GAME
-  break
+
+#----- Dealer's turn -----
+
+if dealer_total == 21
+  puts "Dealer hit blackjack. You lose."
+  exit
 end
 
+while dealer_total < 17
+  #hit
+  new_card = deck.pop
+  puts "Dealing new card for dealer: #{new_card}"
+  dealer_hand << new_card
+  dealer_total = calculate_total(dealer_hand)
+  puts "Dealer TOTAL: #{dealer_total}"
 
+  if dealer_total == 21
+    puts "Sorry, dealer hit blackjack. You lose."
+    exit
+  elsif dealer_total > 21
+    puts "Congrats, dealer busted! YOU WIN!"
+    exit
+  end
+end
 
+#----- compare hands -----
+
+puts "Dealer's cards: "
+dealer_hand.each { |card| puts "=> #{card}" }
+puts ''
+puts "Your cards: "
+player_hand.each { |card| puts "=> #{card}"}
+puts ''
+
+if dealer_total > player_total
+  puts "Sorry, dealer wins."
+elsif dealer_total < player_total
+  puts "YOU WIN!"
+else
+  puts "TIE"
+end
+exit
--- course1/test.rb
+++ course1/test.rb
@@ -2,16 +2,3 @@
 #
 # this is a test file I use to practice multi-line code or isolate bugs
 
-arr = 
-   [[1, 2, 3, 4], 
-    [8, 9, 10], 
-    [10, 2]]
-arr.each do |e|
-    if e.length > 3
-      e.select! { |f| f.even? }
-    else
-      e.map { |f| f * 10 }
-    end
-end
-
-puts arr
